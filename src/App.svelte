8789f48e7820f815c90314fe1f40932ae0ddc815
Shay Forde <shayforde@gmail.com>
2022-06-21 20:37:01 +0100

    finished tutorial 7

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,43 @@
 <script>
-	export let name;
 
-	const changeName = () => {
-		name="Another great title/name"
-		return name;
+	/*
+		https://www.youtube.com/watch?v=9PfCZFqYsYA&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=8
+	*/
+ let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+   ];
+
+	const handleClick = (id) => {
+		people= people.filter(person=> person.id != id )
 	}
-
 	
 </script>
-
-<div class="index">
-	<h1>{name}</h1>
-	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam eos, suscipit tempore placeat optio itaque magnam illum quidem voluptas molestiae sed voluptates debitis laborum eligendi, vel veniam culpa. Atque, doloremque!</p>
-	<button on:click={changeName}>Change name</button>
+<div class="main">
+{#each people as person (person.id)}
+<div >
+  <h4>{person.name}</h4>
+  <p>{person.age} years old,<span > {person.beltColour} </span>belt.</p>
+  <button on:click={()=>handleClick(person.id)}>delete</button>
+</div>
+{:else}
+<p>There are no people to show...</p>
+{/each}
 </div>
 
-
 <style>
-.index{
-	text-align:center;
-	display:block;
-	margin: 20px auto;
-}
+	
+		.main {
+			
+			text-align: center;
+		}
+		h1 {
+			color: #ff3e00;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
+	
+
 </style>
