18183b47f6db3e7ec9f2014797121a7e464b54d5
shayforde <shayforde@gmail.com>
2022-06-22 11:11:07 +0100

    in dialysis finished tutorial 9

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+	import Modal from './Modal.svelte';
+
 
 	/*
 		https://www.youtube.com/watch?v=9PfCZFqYsYA&list=PL4cUxeGkcC9hlbrVO_2QFVqVPhlZmz7tO&index=8
@@ -6,29 +8,25 @@
  let people = [
     { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
-    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+    { name: 'luigi', beltColour: 'black', age: 35, id: 3 }
    ];
 
 	const handleClick = (id) => {
 		people= people.filter(person=> person.id != id )
 	}
-	let num  = 15;
+	
 </script>
-
+<Modal />
 <div class="main">
 
-
-{#if num > 20}
-<p>Number is greater than 20</p>
-{:else if num > 5}
-	<p>Number is greater than 5</p>
-	{:else}
-	<p>Not greater then 5</p>
-{/if}
 
 {#each people as person (person.id)}
 <div >
   <h4>{person.name}</h4>
+  {#if person.beltColour === 'black'}
+	<p><strong>MASTER NINJA</strong></p>
+  {/if}
+
   <p>{person.age} years old,<span > {person.beltColour} </span>belt.</p>
   <button on:click={()=>handleClick(person.id)}>delete</button>
 </div>
---
+++ src/Modal.svelte
@@ -0,0 +1,18 @@
+<script>
+let showModal = true;
+
+</script>
+
+{#if showModal}
+<div class="backdrop">
+    <div class="modal">
+        <p>Signup for offers!<p>
+    </div>
+</div>
+{/if}
+
+
+<style>
+
+
+</style>
